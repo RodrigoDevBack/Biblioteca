f87006fcac2f8e8c5e0100d12740308d3b81763d
RodrigoDevBack <rodrigomoraes3040@gmail.com>
2025-01-27 22:02:23 -0300

    Atualização de lógica nas nomeações

--- src/Tests/EstanteCrudTest.java
+++ src/Tests/EstanteCrudTest.java
@@ -6,29 +6,29 @@ import java.util.Scanner;
 
 public class EstanteCrudTest {
     public static void main(String[] args) {
-        EstanteCrud biblioteca = new EstanteCrud();
-        Scanner inpt = new Scanner(System.in);
+        EstanteCrud estante = new EstanteCrud();
+        Scanner scanner = new Scanner(System.in);
         int opcaoEscolhida;
         do {
             System.out.println("------------------------------------------");
             System.out.println("Bem vindo a estante! \nOpções: \n[1]Adicionar um livro \n[2]Ver livros \n[3]Ver livro específico \n[4]Atualizar livro \n[5]Deletar \n[0]Sair");
             System.out.println("Digite sua escolha: ");
-            opcaoEscolhida = inpt.nextInt();
+            opcaoEscolhida = scanner.nextInt();
             switch (opcaoEscolhida) {
                 case 1:
-                    biblioteca.setLivro();
+                    estante.setBook();
                     break;
                 case 2:
-                    biblioteca.getLivros();
+                    estante.getBooks();
                     break;
                 case 3:
-                    biblioteca.getLivro();
+                    estante.getBook();
                     break;
                 case 4:
-                    biblioteca.upLivro();
+                    estante.upBook();
 
                 case 5:
-                    biblioteca.deleteLivro();
+                    estante.deleteBook();
 
             }
         } while (opcaoEscolhida != 0);
--- src/main/EstanteCrud.java
+++ src/main/EstanteCrud.java
@@ -6,105 +6,109 @@ import java.util.Scanner;
 public class EstanteCrud {
     protected ArrayList<ArrayList<String>> estante = new ArrayList<>();
 
-    protected Scanner inpt = new Scanner(System.in);
+    protected Scanner scanner = new Scanner(System.in);
 
-    public void setLivro() {
-        String nomeLivro;
-        String nomeAutor;
-        String ano;
+    public void setBook() {
+        String nameBook;
+        String nameActor;
+        String year;
         String id;
-        ArrayList<String> livro = new ArrayList<>();
+        ArrayList<String> book = new ArrayList<>();
 
         System.out.println("-----------------------------------------");
-        System.out.println("Nome do livro: ");
-        nomeLivro = this.inpt.nextLine();
+        System.out.println("Nome do book: ");
+        nameBook = this.scanner.nextLine();
         System.out.println("-----------------------------------------");
 
         System.out.println("Autor(a): ");
-        nomeAutor = this.inpt.nextLine();
+        nameActor = this.scanner.nextLine();
         System.out.println("-----------------------------------------");
 
         System.out.println("Ano: ");
-        ano = this.inpt.nextLine();
+        year = this.scanner.nextLine();
         System.out.println("-----------------------------------------");
 
         id = String.valueOf(this.estante.size());
 
-        livro.add(id);
-        livro.add(nomeLivro);
-        livro.add(nomeAutor);
-        livro.add(ano);
+        book.add(id);
+        book.add(nameBook);
+        book.add(nameActor);
+        book.add(year);
 
-        this.estante.add(livro);
+        this.estante.add(book);
         System.out.println("Livro adicionado.");
         System.out.println("-----------------------------------------");
     }
 
-    public void getLivros() {
+    public void getBooks() {
         System.out.println("-----------------------------------------");
         System.out.println("Livros atuais: \n" + this.estante);
         System.out.println("-----------------------------------------");
     }
 
-    public void getLivro() {
-        int id;
+    public void getBook() {
+        String id;
+        int idNew;
 
         System.out.println("-----------------------------------------");
         System.out.println("Digite o id do livro: ");
-        id = this.inpt.nextInt();
+        id = this.scanner.nextLine();
         System.out.println("-----------------------------------------");
-        String livro = String.valueOf(this.estante.get(id));
+        idNew = Integer.parseInt(id);
+
+        String livro = String.valueOf(this.estante.get(idNew));
         System.out.println(livro);
         System.out.println("-----------------------------------------");
     }
 
-    public void upLivro() {
+    public void upBook() {
         String id;
         String escolha;
         String trocar;
-        int idnew;
+        int idNew;
 
         System.out.println("-----------------------------------------");
         System.out.println("Digite o id do livro: ");
-        id = inpt.nextLine();
+        id = scanner.nextLine();
         System.out.println("-----------------------------------------");
 
         System.out.println("O que deseja atualizar? \n[1] Nome do livro \n[2]Autor \n[3]Ano \n[0]Cancelar atualização");
-        escolha = this.inpt.nextLine();
+        escolha = this.scanner.nextLine();
 
-        idnew = Integer.parseInt(id);
+        idNew = Integer.parseInt(id);
 
         switch (escolha) {
             case "0":
                 break;
             case "1":
                 System.out.println("Digite o novo nome do livro: ");
-                trocar = this.inpt.nextLine();
-                this.estante.get(idnew).remove(1);
-                this.estante.get(idnew).listIterator(1).add(trocar);
+                trocar = this.scanner.nextLine();
+                this.estante.get(idNew).remove(1);
+                this.estante.get(idNew).listIterator(1).add(trocar);
             case "2":
                 System.out.println("Digite autor corrigido: ");
-                trocar = this.inpt.nextLine();
-                this.estante.get(idnew).remove(2);
-                this.estante.get(idnew).listIterator(2).add(trocar);
+                trocar = this.scanner.nextLine();
+                this.estante.get(idNew).remove(2);
+                this.estante.get(idNew).listIterator(2).add(trocar);
             case "3":
                 System.out.println("Digite o ano correto: ");
-                trocar = this.inpt.nextLine();
-                this.estante.get(idnew).remove(3);
-                this.estante.get(idnew).listIterator(3).add(trocar);
+                trocar = this.scanner.nextLine();
+                this.estante.get(idNew).remove(3);
+                this.estante.get(idNew).listIterator(3).add(trocar);
         }
 
     }
 
-    public void deleteLivro() {
+    public void deleteBook() {
         String id;
-        int idnew;
+        int idNew;
+
         System.out.println("Digite o livro a ser deletado: ");
-        id = inpt.nextLine();
-        idnew = Integer.parseInt(id);
-        estante.remove(idnew);
-        if (idnew < estante.size()) {
-            for (int i = idnew; i <= (estante.size() - 1); i++) {
+        id = scanner.nextLine();
+        idNew = Integer.parseInt(id);
+        estante.remove(idNew);
+        if (idNew < estante.size()) {
+            for (int i = idNew; i <= (estante.size() - 1); i++) {
                 estante.get(i).removeFirst();
                 estante.get(i).listIterator(0).add(String.valueOf(i));
             }
